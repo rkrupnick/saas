7cec7df9ec3f8501056db8f3799ad08987728cf0
rkrupnick <rachel@rachelkrupnick.com>
2015-09-06 19:10:44 +0000

    Typo fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,5 +18,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
       flash[:notice] = "Please select a membership plan to sign up."
       redirect_to root_url
     end
-  end
 end
