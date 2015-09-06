eabd4eaed4fbf86fef36f3a83b3ed5bb5deebe06
rkrupnick <rachel@rachelkrupnick.com>
2015-09-06 19:03:41 +0000

    Udate routes with new devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'pages#home' 
   get '/about' => 'pages#about'
   resources :contacts
