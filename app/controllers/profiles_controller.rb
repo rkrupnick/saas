48529f6c9d65a156b410649b29e3ace0862a8724
rkrupnick <rachel@rachelkrupnick.com>
2015-09-10 03:23:36 +0000

    Add before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :only_current_user
+    
     def new
        @user = User.find(params[:user_id]) 
        @profile = Profile.new
@@ -20,6 +23,19 @@ class ProfilesController < ApplicationController
        @profile = @user.profile
     end
     
+    
+    def update
+  @user = User.find( params[:user_id] )
+  @profile = @user.profile
+  if @profile.update_attributes(profile_params)
+    flash[:success] = "Profile Updated!"
+    redirect_to user_path( params[:user_id] )
+  else
+    render action: :edit
+  end
+   end
+
+
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
